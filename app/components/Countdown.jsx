825ebf9cec1f9b7269c9682b17b0eb845c85dd9d
kenny <kenny.selorm@gmail.com>
2017-02-10 08:12:41 -0800

    countdown tested and working

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,47 @@
+var React = require('react');
+var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
+
+var Countdown = React.createClass({
+  getInitialState: function () {
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    };
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0 //set newCount to zero if the condition fails
+      });
+    }, 1000);  //1000 milliseconds
+  },
+  handleSetCountdown: function (seconds) {
+    this.setState({
+      count: seconds,
+      countdownStatus: 'started'
+    });
+  },
+  render: function () {
+    var {count} = this.state;
+
+    return (
+      <div>
+        <Clock totalSeconds={count}/>
+        <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+      </div>
+    );
+  }
+});
+
+module.exports = Countdown;
